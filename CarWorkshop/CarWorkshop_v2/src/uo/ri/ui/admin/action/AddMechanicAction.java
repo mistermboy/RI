4a9bdc4667895a67d55ca2d896a00fdbc694796a
Pablo Menéndez Suárez <pabloyo97@hotmail.com>
2017-11-14 13:52:17 +0100

    v0/v1/v2

---
+++ CarWorkshop/CarWorkshop_v2/src/uo/ri/ui/admin/action/AddMechanicAction.java
@@ -0,0 +1,23 @@
+package uo.ri.ui.admin.action;
+
+import alb.util.console.Console;
+import alb.util.menu.Action;
+import uo.ri.business.admin.AddMechanic;
+import uo.ri.common.BusinessException;
+
+public class AddMechanicAction implements Action {
+
+	@Override
+	public void execute() throws BusinessException {
+
+		// Pedir datos
+		String nombre = Console.readString("Nombre");
+		String apellidos = Console.readString("Apellidos");
+
+		new AddMechanic(nombre, apellidos).execute();
+
+		// Mostrar resultado
+		Console.println("Nuevo mecánico añadido");
+	}
+
+}
---
+++ CarWorkshop/CarWorkshop_v2/src/uo/ri/ui/cash/action/FacturarReparacionesAction.java
@@ -0,0 +1,36 @@
+package uo.ri.ui.cash.action;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import alb.util.console.Console;
+import alb.util.menu.Action;
+import uo.ri.business.cash.CreateInvoiceFor;
+import uo.ri.common.BusinessException;
+
+public class FacturarReparacionesAction implements Action {
+
+	
+	public FacturarReparacionesAction() {
+		
+	}
+	
+	@Override
+	public void execute() throws BusinessException {
+		List<Long> idsAveria = new ArrayList<Long>();
+		
+		do {
+			Long id = Console.readLong("ID de averia");
+			idsAveria.add(id);
+		} while (masAverias());
+		
+		CreateInvoiceFor c = new CreateInvoiceFor(idsAveria);
+		idsAveria = c.execute();
+		
+	}
+
+	private boolean masAverias() {
+		return Console.readString("¿Añadir más averias? (s/n) ").equalsIgnoreCase("s");
+	}
+
+}
